7240dfc4d6b3bcc8d8eaa79141562d603eedf42a
marek.rojik <marek.rojik@inventi.cz>
2019-05-04 15:34:09 +0200

    feat: modify run.sh script

--- backend/run.sh
+++ backend/run.sh
@@ -1,1 +1,12 @@
-java -jar -Dserver.port=8080 -Dspring.profiles.active=prod -Djava.security.egd=file:/dev/urandom microbenchmark-web/target/microbenchmark.jar &
+#!/bin/bash
+
+if ! [ -x "$(command -v docker)" ]; then
+    echo "Docker is not found. Please install Docker."
+
+elif ! [ -x "$(command -v psql)" ]; then
+    echo "PostgreSQL is not found. Please install PostgreSQL."
+
+else
+    echo "Run MBMark application on port 8080."
+    java -jar -Dserver.port=8080 -Dspring.profiles.active=prod -Djava.security.egd=file:/dev/urandom microbenchmark-web/target/microbenchmark.jar &
+fi
